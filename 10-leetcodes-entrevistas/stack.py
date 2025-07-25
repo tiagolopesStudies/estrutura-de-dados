a10d75e0333a3fec2a5e3331d1a217c6d521f64f
Tiago Lopes <tiagolopes@cacdigital.com.br>
2025-07-25 10:04:36 -0300

    adicionando exercício de pilhas (stack)

---
+++ 10-leetcodes-entrevistas/stack.py
@@ -0,0 +1,32 @@
+# Exercise: Min Stack
+
+class MinStack:
+  def __init__(self):
+    self.stack     = []
+    self.min_stack = []
+
+  def push(self, val: int) -> None:
+    self.stack.append(val)
+    if self.min_stack:
+      val = min(self.min_stack[-1], val)
+    self.min_stack.append(val)
+
+  def pop(self) -> None:
+    self.stack.pop()
+    self.min_stack.pop()
+
+  def top(self) -> int:
+    return self.stack[-1]
+
+  def getMin(self) -> int:
+    return self.min_stack[-1]
+
+minStack = MinStack()
+
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())
+minStack.pop()
+print(minStack.top())
+print(minStack.getMin())
